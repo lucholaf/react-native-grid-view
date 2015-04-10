599c092017c1e8c212ed478535949750230c4a91
Luis Floreani <luis.floreani@gmail.com>
2015-04-10 16:07:55 -0300

    added example

---
+++ Examples/CollectionView/index.ios.js
@@ -0,0 +1,136 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var CollectionView = require('react-native-grid-view');
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+var MOVIES_PER_ROW = 3;
+
+var Movie = React.createClass({
+  render: function() {
+      return <View style={styles.element}>
+        <Image
+          source={{uri: this.props.movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View >
+          <Text 
+          style={styles.title}
+          numberOfLines={3}>{this.props.movie.title}</Text>
+          <Text style={styles.year}>{this.props.movie.year}</Text>
+        </View>
+      </View>
+  },
+});
+
+var AwesomeProject = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: null,
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: responseData.movies,
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <CollectionView
+        items={this.state.dataSource}
+        itemsPerRow={MOVIES_PER_ROW}
+        renderRow={this.renderMoviesGroup}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.group}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMoviesGroup: function(group) {
+    var movies = group.map(function(movie) {
+      return <Movie movie={movie}/>
+    });
+    return (
+      <View style={styles.group}>
+        {movies}
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  group: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    height: 170,
+  },
+  element: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 10,
+    marginBottom: 8,
+    width: 90,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
