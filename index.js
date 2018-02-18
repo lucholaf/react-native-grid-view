4c64fa28905a39f26a08c62d9674e931ea27c7a3
Michael Malura <maluramichael@googlemail.com>
2018-02-18 11:24:26 +0100

    Update code to be compatible with the latest react-native version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   AppRegistry,
@@ -9,47 +9,48 @@ import {
   ListView,
 } from 'react-native';
 
-var CollectionView = React.createClass({
-    groupItems: function(items, itemsPerRow) {
-        var itemsGroups = [];
-        var group = [];
-        items.forEach(function(item) {
-          if (group.length === itemsPerRow) {
-            itemsGroups.push(group);
-            group = [item];
-          } else {
-            group.push(item);
-          }
-        });
-
-        if (group.length > 0) {
+class CollectionView extends Component {
+  groupItems = function(items, itemsPerRow) {
+      var itemsGroups = [];
+      var group = [];
+      items.forEach(function(item) {
+        if (group.length === itemsPerRow) {
           itemsGroups.push(group);
+          group = [item];
+        } else {
+          group.push(item);
         }
-
-        return itemsGroups;
-    },
-    renderGroup: function(group) {
-      var that = this;
-      var items = group.map(function(item, index) {
-        return that.props.renderItem(item, index);
       });
-      return (
-        <View style={styles.group}>
-          {items}
-        </View>
-      );
-    },
-    render: function() {
-        var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
-        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        return (<ListView
-          {...this.props}
-          renderRow={this.renderGroup}
-          dataSource={ds.cloneWithRows(groups)}
-        />);
-    },
-});
 
+      if (group.length > 0) {
+        itemsGroups.push(group);
+      }
+
+      return itemsGroups;
+  }
+
+  renderGroup = function(group) {
+    var that = this;
+    var items = group.map(function(item, index) {
+      return that.props.renderItem(item, index);
+    });
+    return (
+      <View style={styles.group}>
+        {items}
+      </View>
+    );
+  }
+
+  render = function() {
+      var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      return (<ListView
+        {...this.props}
+        renderRow={this.renderGroup}
+        dataSource={ds.cloneWithRows(groups)}
+      />);
+  }
+}
 
 var styles = StyleSheet.create({
   group: {
